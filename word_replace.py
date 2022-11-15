ab414c5864e1a748eaf11f0146cef3ecc73ff837
Abhishek Singh <110656538+ijabhishek@users.noreply.github.com>
2022-11-15 22:25:33 +0530

    Add files via upload

---
+++ word_replace.py
@@ -0,0 +1,8 @@
+def replace_word():
+
+    str = "Hi I am abhishek and how are you?"
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacement = input("Enter the word to replacement: ")
+    print(str.replace(word_to_replace, word_replacement))
+
+replace_word() 
