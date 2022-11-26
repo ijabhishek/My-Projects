fb3c4731e4d515ba31b78d9d5e38796814ed2539
Abhishek Singh <110656538+ijabhishek@users.noreply.github.com>
2022-11-26 23:29:06 +0530

    Add files via upload

---
+++ guess_game.py
@@ -0,0 +1,18 @@
+import random
+"""This game is gussenig by us by giving input number to the computer"""
+# def guess(x):
+#     random_number = random.randint(1,x)
+#     guess = 0
+#     while guess != random_number:
+#         guess = int(input(f'Guess a number between 1 and {x}:'))
+#         if guess < random_number:
+#             print('sorry, guess again , Too low')
+#         elif guess > random_number:
+#             print('Sorry , guess again , Too high')
+    
+#     print(f'Yay, congrats, you have guessed the number {random_number} correctly!!')
+
+# guess(10)
+
+
+
